00b358dca72751adf9437b724cc4bfcb7ed0979d
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-20 15:48:11 +0530

    feat: initialize React form project with Next.js and Tailwind CSS
    - Added package.json with dependencies for Next.js, React, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for UI components.
    - Implemented global styles with Tailwind CSS and custom CSS variables.
    - Set up root layout and main page structure for the application.

---
+++ React-Form/form/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+import { Form } from "./components/form";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+      <Form />
+      
+    </div>
+  );
+}
