3cc425a6b516df1e2e803bc17dcfb27eef67b61a
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-22 21:46:46 +0530

    fix: correct import statement for Form component and update styling

--- React-Form/form/src/app/page.js
+++ React-Form/form/src/app/page.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Form } from "./components/form";
+import { form as Form } from "./components/form";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans text-gray-950">
       <Form />
-      
+      {/* <h2>Hello</h2> */}
     </div>
   );
 }
