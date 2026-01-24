e8d1207821e8463e785ae464a9f5a7676dc1f0b3
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-24 19:40:39 +0530

    fix: made neccessary changes inorder to make the form component visible

--- React-Form/form/src/app/page.js
+++ React-Form/form/src/app/page.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans text-gray-950">
       <Form />
-      {/* <h2>Hello</h2> */}
     </div>
   );
 }
