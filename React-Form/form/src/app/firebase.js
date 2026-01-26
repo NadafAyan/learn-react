bd4787605b57dff09e21330408888733f8bb4aa3
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-26 21:53:35 +0530

    feat: add Firebase initialization and configuration

---
+++ React-Form/form/src/app/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDsVGbZU03wbQqkxs9tEZ7YObDUxv89HMo",
+  authDomain: "learn-firebase-c9e31.firebaseapp.com",
+  projectId: "learn-firebase-c9e31",
+  storageBucket: "learn-firebase-c9e31.firebasestorage.app",
+  messagingSenderId: "817112338753",
+  appId: "1:817112338753:web:1bdc5e3573e41baaa6d1c0",
+  measurementId: "G-Z59W4QQYCY"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
