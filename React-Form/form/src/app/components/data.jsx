e23d9bf294068797ab1ff3b49a0375a9716db150
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-27 22:27:34 +0530

    fix: enhance layout and styling in Data component for better presentation

--- React-Form/form/src/app/components/data.jsx
+++ React-Form/form/src/app/components/data.jsx
@@ -19,11 +19,11 @@ export const Data = () => {
     }, []);
 
   return (
-    <div>
-      <h2>Submissions</h2>
-      <ul>
+    <div className='mt-8 p-6 bg-white rounded-md shadow-md w-full max-w-md'>
+      <h2 className='text-xl font-semibold mb-4'>Submissions</h2>
+      <ul className='list-disc list-inside'>
         {submissions.map(submission => (
-          <li key={submission.id}>
+          <li key={submission.id} className='mb-2'>
             Name: {submission.name}, Email: {submission.email}
           </li>
         ))}
