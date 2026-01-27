8f0f5eac26d52f2f0d9d67854fb99b6e9558a952
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-27 22:22:52 +0530

    feat: implement Data component to fetch and display submissions from Firestore

--- React-Form/form/src/app/components/data.jsx
+++ React-Form/form/src/app/components/data.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+import { React, useEffect, useState } from 'react';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { app } from "../firebase";
+const firestore = getFirestore(app);
 
 export const Data = () => {
+
+    const [submissions, setSubmissions] = useState([]);
+
+    const fetchData = async () => {
+        const querySnapshot = await getDocs(collection(firestore, "submissions"));
+        const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setSubmissions(data);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
   return (
     <div>
-        
+      <h2>Submissions</h2>
+      <ul>
+        {submissions.map(submission => (
+          <li key={submission.id}>
+            Name: {submission.name}, Email: {submission.email}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
