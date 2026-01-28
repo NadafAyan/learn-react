1642fb09cfd9aebb023ffe251805d4a88dddbdbe
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-28 22:33:39 +0530

    feat: tested the tailwind funtionalities.

--- hooks/src/components/counter.jsx
+++ hooks/src/components/counter.jsx
@@ -4,11 +4,12 @@ import './counter.css';
 const Counter = () => {
     const [ count, setCount ] = useState(0);
   return (
-    <div className="counter-container">
+    <div className="align-center text-center border-2 border-blue-500 p-4 m-4">
         <p className='para'>You have clicked {count} times.</p>
         <button className='btn' onClick={() => {
             setCount(count + 1);
         }}>Click me</button>
+        <h1 className='text-9xl underline'>Ayan Nadaf</h1>
     </div>
   )
 }
