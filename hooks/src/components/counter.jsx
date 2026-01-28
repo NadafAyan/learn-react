31bf54e9a0c8dac20ac2110f83eb23776d6a28ac
Ayan Nadaf <nadafayan95@gmail.com>
2026-01-28 22:26:11 +0530

    fix: format code and add click functionality to Counter button

--- hooks/src/components/counter.jsx
+++ hooks/src/components/counter.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import './counter.css';
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+    const [ count, setCount ] = useState(0);
   return (
     <div className="counter-container">
         <p className='para'>You have clicked {count} times.</p>
-        <button className='btn'>Click me</button>
+        <button className='btn' onClick={() => {
+            setCount(count + 1);
+        }}>Click me</button>
     </div>
   )
 }
